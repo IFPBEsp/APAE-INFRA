f07933efe649fbb098f56741e19b1fc85960bcf7
luiz <luiz.artur.coder@gmail.com>
2026-09-08 22:49:21 -0300

    test: valida erro de formatacao

--- terraform/environments/ci-test/main.tf
+++ terraform/environments/ci-test/main.tf
@@ -1,12 +1,1 @@
-terraform {
-  required_version = ">= 1.0"
-}
-
-variable "message" {
-  type    = string
-  default = "APAE"
-}
-
-output "message" {
-  value = var.message
-}
+variable "message" {type=string}
