e6ddfda4bad77d885b9e30f2e08f65f91a080793
luiz <luiz.artur.coder@gmail.com>
2026-09-08 22:51:46 -0300

    test: testa erro de validacao

--- terraform/environments/ci-test/main.tf
+++ terraform/environments/ci-test/main.tf
@@ -1,1 +1,12 @@
-variable "message" {type=string}
+terraform {
+  required_version = ">= 1.0"
+}
+
+variable "message" {
+  type    = string
+  default = "APAE"
+}
+
+output "message" {
+  value = var.does_not_exist
+}
